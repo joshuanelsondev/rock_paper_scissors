279e58f41de5dfa1301fc07fa292bf9d08c02178
Joshua Nelson <joshuanelson894@gmail.com>
2022-05-17 19:45:19 -0400

    Write computerPlay function
    randomize the output of either rock, paper, or scissors

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,3 +1,10 @@
 function computerPlay() {
-    
+   play = Math.floor(Math.random() * 3);
+   if (play == 0) {
+       return console.log("Rock");
+   } else if (play == 1) {
+       return console.log("Paper");
+   } else {
+       return console.log("Scissors");
+   }
 }
