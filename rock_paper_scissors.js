62934686a05bf3ffdddf28d2a03b653b283bf5f2
Joshua Nelson <joshuanelson894@gmail.com>
2022-05-17 23:04:10 -0400

    Write a function to be used by singleRound
    function to decide the winner

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,10 +1,52 @@
+// Computer picks randomly, rock, paper, or scissors
 function computerPlay() {
+
    play = Math.floor(Math.random() * 3);
+  
    if (play == 0) {
-       return console.log("Rock");
+       return console.log("rock");
    } else if (play == 1) {
-       return console.log("Paper");
+       return console.log("paper");
    } else {
-       return console.log("Scissors");
+       return console.log("scissors");
    }
+}
+
+/* A single round of RPS. 
+Prompt the user for their selection, be sure to use toLowerCase for case-insensitive response
+Call computerPlay to get the computer's selection
+Use each selection as parameters
+Compare the selections using <If> statements
+Return a string that determines the winner of the round
+*/
+
+
+function singleRound(playerSelection, computerSelection) {
+   if (playerSelection == computerSelection) {
+       return "This round is a tie";
+    } 
+}
+
+const playerSelection = prompt("Choose 'Rock', 'Paper', or 'Scissors': ").toLowerCase;
+const computerSelection = computerPlay();
+console.log(singleRound(playerSelection, computerSelection));
+
+function winner(player, computer) {
+    let win = "";
+    
+    if ((player == "rock") && (computer == "scissors")) {
+        win = player;
+    } else if ((player == "paper") && (computer == "rock")) {
+        win = player;
+    } else if ((player == "scissors") && (computer == "paper")) {
+        win = player;
+    } else if ((player == "rock") && (computer == "paper")) {
+        win = computer;
+    } else if ((player == "scissors") && ("rock")) {
+        win = computer;
+    } else if ((player == "paper") && (computer == "scissors")) {
+        win = computer;
+    }
+        
+    return win;
 }
