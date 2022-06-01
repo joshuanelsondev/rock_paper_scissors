31c3aa5167c05a3a8681131dd267463aa60ecfca
Joshua Nelson <joshuanelson894@gmail.com>
2022-06-01 12:50:44 -0400

    Add comments

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,77 +1,7 @@
 
 
-/* A single round of RPS. 
-Prompt the user for their selection, be sure to use toLowerCase for case-insensitive response
-Call computerPlay to get the computer's selection
-Use each selection as parameters
-Compare the selections using <If> statements
-Return a string that determines the winner of the round
-*/
+// Variables to be used from html
 
-// Write a function for playing one round of RPS
-function playRound(player, computer) {
-  
-    if (player == computer) {
-        console.log(`Player score: ${playerScore}, Computer score: ${computerScore}`); 
-        return "This round is a tie";
-    } 
-
-    if (winner(player, computer) == player) {
-        playerScore += 1;
-        console.log(`Player score: ${playerScore}, Computer score: ${computerScore}`);
-        return `You Win! ${player} beats ${computer}!`;
-    } else {
-        computerScore += 1;
-        console.log(`Player score: ${playerScore}, Computer score: ${computerScore}`);
-        return `You Lose, ${computer} beats ${player}.`;
-    }
-}
-
-
-// Create a function 'game()', call 'playRound' five times to create a 5-round game of RPS
-
-const game = () => {
-   
-    for (let i = 0; i < 5; i++) {
-        console.log(playRound(playerSelection(), computerPlay())); 
-        console.log(`Player score: ${playerScore}, Computer score: ${computerScore}`);   
-    }
-
-    if (playerScore > computerScore) {
-        return "WINNER!";
-    } else if (playerScore < computerScore) {
-        return "Sorry, you lose...play again!";
-    } else {
-        return "It's a tie!";
-    }
-};
-
-// Create a function that prompts the user to pick between criteria; prompt user until answer fits criteria via a while loop
-const playerSelection = () => {
-    let x = 0;
-    let choose = prompt("Choose 'Rock', 'Paper', or 'Scissors': ").toLowerCase();
-   
-    while (x == 0) {
-
-        if ((choose == "rock") || (choose == "paper") || (choose == "scissors")) {
-            x++;
-            return choose;    
-        } else {
-            choose = prompt("Invalid entry, please enter 'Rock', 'Paper', or 'Scissors': ").toLowerCase();
-            x = 0;
-        }
-        
-    } 
-};
-
-
-
-
-
-
-// console.log(game());
-
-// DOM 
 const playerResult = document.getElementById('player');
 const playerScoreDisplay = document.getElementById('player-score');
 const computerResult = document.getElementById('computer');
@@ -86,6 +16,8 @@ let result;
 let playerScore = 0;
 let computerScore = 0;
 
+// A round is played when the user clicks on their choice
+
 playerButton.forEach(playerChoice => playerChoice.addEventListener('click', playGame));
 
 function playGame(e) {
@@ -98,6 +30,7 @@ function playGame(e) {
 }
 
 // Computer picks randomly, rock, paper, or scissors
+
 function computerPlay() {
     play = Math.floor(Math.random() * 3);
 
@@ -112,6 +45,7 @@ function computerPlay() {
 
 }
 // Write a helper function that will determine which item beats the other
+
 function winner() {
     player = playerResult.textContent;
     computer = computerChoice;
@@ -151,6 +85,8 @@ function winner() {
         gameOver();
     }
 }
+
+// Create a button that appears when the game is over and refreshes the page
 
 function gameOver() {
     const restartButton = document.createElement('button');
